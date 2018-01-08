927e831f23eaf91572f0a219be1e84cdaf25fdfd
Daniel Merrill <daniel.r.merrill@gmail.com>
2018-01-07 19:37:06 -0800

    final no persist

--- App.js
+++ App.js
@@ -2,26 +2,13 @@ import React, { Component } from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import logger from 'redux-logger'
+
 import navigation from './src/reducer'
-import { persistStore, persistCombineReducers } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // default: localStorage if web, AsyncStorage if react-native
 
 import Navigator from './src/Navigator'
 
-const config = {
-  key: 'primary',
-  storage
- }
-
-const reducer = persistCombineReducers(config, { navigation })
+const reducer = combineReducers({ navigation })
 const store = createStore(reducer, applyMiddleware(logger))
-persistStore(
-  store,
-  null,
-  () => {
-    store.getState() // if you want to get restoredState
-  }
-)
 
 export default class App extends Component {
   render() {
