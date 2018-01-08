1d90049dbfa2a6604765e662fa736636d55b3ce3
Daniel Merrill <daniel.r.merrill@gmail.com>
2018-01-07 22:26:29 -0800

    remove Profile

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { StackNavigator, addNavigationHelpers } from 'react-navigation'
 
-import Profile from './components/Profile'
 import Feed from './components/Feed'
 import ItemDetail from './components/ItemDetail'
 
 export const Navigator = new StackNavigator({
-  Profile: { screen: Profile },
   Feed: { screen: Feed },
   ItemDetail: { screen: ItemDetail },
 },{
